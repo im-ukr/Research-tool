da370a60d72f0277b664065c7d39c2b680915054
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-05-09 02:31:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+import os
+import streamlit as st
+import pickle
+import time
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+
+from openapi_key import openapi_key
+os.environ['OPENAI_API_KEY'] = openapi_key
+
+st.title("News Research Tool 📈")
+st.sidebar.title("News Article URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+process_url_clicked = st.sidebar.button("Process URLs")
+file_path = "faiss_store_openai.pkl"
+
+main_placeholder = st.empty()
+llm = OpenAI(temperature=0.9, max_tokens=500)
+
+if process_url_clicked:
+    # load data
+    loader = UnstructuredURLLoader(urls=urls)
+    main_placeholder.text("Data Loading...Started...✅✅✅")
+    data = loader.load()
+    # split data
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=['\n\n', '\n', '.', ','],
+        chunk_size=1000
+    )
+    main_placeholder.text("Text Splitter...Started...✅✅✅")
+    docs = text_splitter.split_documents(data)
+    # create embeddings and save it to FAISS index
+    embeddings = [llm(doc) for doc in docs]
+    main_placeholder.text("Embedding Vector Started Building...✅✅✅")
+    time.sleep(2)
+
+    # Save the FAISS index to a pickle file
+    vectorstore_openai = FAISS.from_embeddings(embeddings)
+    with open(file_path, "wb") as f:
+        pickle.dump(vectorstore_openai, f)
+
+query = main_placeholder.text_input("Question: ")
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=vectorstore.as_retriever())
+            result = chain({"question": query}, return_only_outputs=True)
+            # result will be a dictionary of this format --> {"answer": "", "sources": [] }
+            st.header("Answer")
+            st.write(result["answer"])
+
+            # Display sources, if available
+            sources = result.get("sources", "")
+            if sources:
+                st.subheader("Sources:")
+                sources_list = sources.split("\n")  # Split the sources by newline
+                for source in sources_list:
+                    st.write(source)
